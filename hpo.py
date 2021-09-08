d52a22e693236643d52699fa2434004dd5182113
Amit Lathiya <lathiyaamit@gmail.com>
2021-09-07 23:26:36 -0400

    correct syntax error in script file

--- hpo.py
+++ hpo.py
@@ -7,31 +7,96 @@ import torch.optim as optim
 import torchvision
 import torchvision.models as models
 import torchvision.transforms as transforms
-
+import torch.nn.functional as F
+from torchvision import datasets, transforms, models
+from collections import OrderedDict
 import argparse
+import os
+import logging
+import sys
+from PIL import ImageFile
 
-def test(model, test_loader):
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
+def test(model, test_loader, criterion):
     '''
     TODO: Complete this function that can take a model and a 
           testing data loader and will get the test accuray/loss of the model
           Remember to include any debugging/profiling hooks that you might need
     '''
-    pass
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            output = model(data)
+            test_loss += criterion(output, target).item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
 
-def train(model, train_loader, criterion, optimizer):
+    test_loss /= len(test_loader.dataset)
+
+    print(
+        "\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
+            test_loss, correct, len(test_loader.dataset), 100.0 * correct / len(test_loader.dataset)
+        )
+    )
+
+def train(model, trainloader, testloader, loss_criterion, optimizer):
     '''
     TODO: Complete this function that can take a model and
           data loaders for training and will get train the model
           Remember to include any debugging/profiling hooks that you might need
     '''
-    pass
+    epochs = 10
+    loss = 0
+
+    for e in range(epochs):
+    
+        # Model in training mode, dropout is on
+        model.train()
+        for batch_idx, (data, target) in enumerate(trainloader):
+            optimizer.zero_grad()
+            output = model(data)
+            loss = loss_criterion(output, target)
+            loss.backward()
+            optimizer.step()
+            
+            # Training Loss
+            loss += loss.item()
+        
+            if batch_idx % 100 == 0:
+                print("\nEpoch: {}/{}.. ".format(e+1, epochs))
+                print("Training Loss: {:.4f}".format(loss/100))
+                test(model, testloader, loss_criterion)
+    
     
 def net():
     '''
     TODO: Complete this function that initializes your model
           Remember to use a pretrained model
     '''
-    pass
+    #pretrained network VGG16 is loaded from torchvision.models
+    model = models.vgg16(pretrained=True)
+
+    # Freeze all the parameters in the feature network 
+    for param in model.parameters():
+        param.require_grad = False 
+
+    # define the architecture for classifier 
+    classifier = nn.Sequential(OrderedDict([('fc1',nn.Linear(25088,4096)), 
+                                        ('relu',nn.ReLU()),
+                                        ('Dropout',nn.Dropout(0.5)),
+                                        ('fc2',nn.Linear(4096,133)),
+                                        ('output',nn.LogSoftmax(dim=1))]))
+
+    # Set the classifier to model 
+    model.classifier = classifier
+    
+    return model
 
 def create_data_loaders(data, batch_size):
     '''
@@ -41,6 +106,32 @@ def create_data_loaders(data, batch_size):
     pass
 
 def main(args):
+    
+    #data_dir = 'dogImages'
+    path = 'pretrained_cnn.pt'
+    
+    train_transforms = transforms.Compose([transforms.RandomRotation(30),
+                                       transforms.RandomHorizontalFlip(),
+                                       transforms.Resize(256),
+                                       transforms.CenterCrop(224),
+                                       transforms.ToTensor(),
+                                       transforms.Normalize([0.485, 0.456, 0.406], 
+                                                            [0.229, 0.224, 0.225])])
+    test_transforms = transforms.Compose([transforms.Resize(256),
+                                      transforms.CenterCrop(224),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize([0.485, 0.456, 0.406], 
+                                                           [0.229, 0.224, 0.225])])
+
+    # TODO: Load the datasets with ImageFolder
+    train_datasets = datasets.ImageFolder(args.traindatadir, transform=train_transforms)
+    test_datasets = datasets.ImageFolder(args.validdatadir, transform=test_transforms)
+
+
+    # TODO: Using the image datasets and the trainforms, define the dataloaders
+    trainloader = torch.utils.data.DataLoader(train_datasets, batch_size=args.batchsize, shuffle=True)
+    testloader = torch.utils.data.DataLoader(test_datasets, batch_size=32, shuffle=False)
+    
     '''
     TODO: Initialize a model by calling the net function
     '''
@@ -49,19 +140,19 @@ def main(args):
     '''
     TODO: Create your loss and optimizer
     '''
-    loss_criterion = None
-    optimizer = None
+    loss_criterion = nn.NLLLoss()
+    optimizer = optim.Adam(model.classifier.parameters(), lr=args.lr)
     
     '''
     TODO: Call the train function to start training your model
     Remember that you will need to set up a way to get training data from S3
     '''
-    model=train(model, train_loader, loss_criterion, optimizer)
+    model=train(model, trainloader, testloader, loss_criterion, optimizer)
     
     '''
     TODO: Test the model to see its accuracy
     '''
-    test(model, test_loader, criterion)
+    #test(model, test_loader, loss_criterion)
     
     '''
     TODO: Save the trained model
@@ -69,11 +160,27 @@ def main(args):
     torch.save(model, path)
 
 if __name__=='__main__':
+    
     parser=argparse.ArgumentParser()
     '''
     TODO: Specify all the hyperparameters you need to use to train your model.
     '''
     
-    args=parser.parse_args()
+    parser.add_argument(
+        "--batchsize",
+        type=int,
+        default=64,
+        metavar="N",
+        help="input batch size for training (default: 64)",
+    )
     
+    parser.add_argument(
+        "--lr", type=float, default=0.1, metavar="LR", help="learning rate (default: 1.0)"
+    )
+    
+    parser.add_argument('--traindatadir', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
+    parser.add_argument('--validdatadir', type=str, default=os.environ['SM_CHANNEL_TEST'])
+    
+    args = parser.parse_args()
+
     main(args)
